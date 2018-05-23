51dbeb56798d2dd9dbed977b09f199c6a21e7f2b
Cory Kitchens <cjkitche@calpoly.edu>
2018-05-23 09:00:29 -0700

    Initial glue of api and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import Main from './containers/Main';
+import 'whatwg-fetch';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    fetch('/api/')
+      .then(resp => resp.json())
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error));
   }
 
   render() {
