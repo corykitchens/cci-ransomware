2fc63ca874be3b818d369c81ce50e213c3990a8a
Cory Kitchens <corykitchens@gmail.com>
2018-06-10 15:00:47 -0700

    Begin Admin View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 import Login from './containers/Login';
 import Rules from './containers/Rules';
 import Main from './containers/Main';
+import AdminView from './containers/AdminView';
 import './components/CountdownClock.css';
 import 'whatwg-fetch';
 
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <Router>
         <Switch>
+          <Route path="/admin" component={AdminView} />
           <Route path="/main" component={Main} />
           <Route path="/rules" component={Rules} />
           <Route path="/" component={Login} />
