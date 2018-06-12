ebbdd0dfe73093e25fd55327f8b8185010cce684
Cory Kitchens <cjkitche@calpoly.edu>
2018-06-12 16:31:28 -0700

    Use green color for clock

--- client/src/containers/Rules.js
+++ client/src/containers/Rules.js
@@ -7,8 +7,6 @@ import Card from '../components/Card';
 import Columns from '../components/Columns';
 import Column from '../components/Column';
 
-
-
 const Rules = (props) => {
   // Probably a better way to do this
   
@@ -16,13 +14,26 @@ const Rules = (props) => {
     return <Redirect to='/' />
   }
   return (
-    <Container>
+    <Container className="game">
       <Columns>
         <Column>
           <Card className="card-container">
           <Title title="Ransomware" classes={'title has-text-danger has-text-centered'}/>
           <Title title="Rules" classes={'subtitle has-text-danger has-text-centered'} />
             <div className="column is-half is-offset-one-quarter">
+            <h4 className="subtitle">I HAVE TAKEN CONTROL OF ALL PAIENT FILES.</h4>
+            
+            <h4 className="subtitle">YOU HAVE 5 HOURS TO SEND 10 BITCOIN TO THE FOLLOWING ACCOUNT: 1D1HkwCHJvMzhWhZWUQtivprhJDpMwQWWR. </h4>
+
+            <h4 className="subtitle">FAILURE TO DO SO WILL RESULT IN THE DELETION OF ALL MEDICAL RECORDS.</h4>
+
+            <h4 className="subtitle">THIS DEVICE IS SET TO TRIGGER MALWARE THAT WILL INFECT EVERY COMPUTER ON THE HOSPITAL NETWORK. </h4>
+
+
+            <h4 className="subtitle">IF I RECIVEVE MY MONEY IN TIME, I WILL RELEASE THE SIX CODES TO DEFUSE THE DEVICE. </h4>
+
+            <h4 className="subtitle">IF YOU TRY TO STOP IT YOURSELF THE CLOCK WILL SPEED UP. </h4>
+
               <ul className="rules-list">
                 <li>You must find 5 hidden passwords</li>
                 <li>You are given 5 hours to find all 6 passwords</li>
